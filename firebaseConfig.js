2efec02e570ae1da3f11d4870848d9bd7c9fc985
Anurag Tiwari <anurag07t@gmail.com>
2026-05-13 10:31:36 +0530

    Remove Firebase configuration details

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -8,13 +8,6 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCIqmjYQoh9XZD4t_NKn3Fr26bRbNoKS8U",
-  authDomain: "natpactravelapp.firebaseapp.com",
-  projectId: "natpactravelapp",
-  storageBucket: "natpactravelapp.firebasestorage.app",
-  messagingSenderId: "81146906946",
-  appId: "1:81146906946:web:41c13691c4dcaf9d2ce0db",
-  measurementId: "G-BVZ6VHP0CF"
 };
 
 // Initialize Firebase
